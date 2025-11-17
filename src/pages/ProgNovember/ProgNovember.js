446829603c2bc0b3c956cd304c16f59efd9d0ee0
FredericGuerin <fguerin555@gmail.com>
2025-11-17 10:46:57 +0100

    Video Ceramica GOOD

--- src/pages/ProgNovember/ProgNovember.js
+++ src/pages/ProgNovember/ProgNovember.js
@@ -690,7 +690,7 @@ const ProgNovember = () => {
         </div>
         <div className={styles.videoCeramica}>
           <iframe
-            src="https://www.youtube.com/embed/Vv94m_YSccc"
+            src="https://www.youtube.com/embed/-Vaic0UODUo"
             title="Ceramica"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
